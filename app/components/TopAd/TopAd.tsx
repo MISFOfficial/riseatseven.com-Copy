d6838404da0bb547e2dacbd970af9484fcc2dcf4
MISFOfficial <muksitul44@gmail.com>
2026-05-04 18:44:10 +0600

    feat: add TopAd component to homepage header

---
+++ app/components/TopAd/TopAd.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function TopAd() {
+  return (
+    <a
+      href=""
+      className="group flex justify-center z-60 relative items-center text-xs w-full py-2 px-5 text-balance stext-center tracking-tight leading-none font-semibold rounded-2xl transition transition-rounded | lg:text-sm | pointer-fine:hover:rounded-md text-grey-900 bg-mint"
+    >
+      <div className="block mt-0.5 | lg:hidden">
+        🚨 Where are your customers actually searching? Download the report
+      </div>
+
+      <div className="relative overflow-hidden mt-0.5 hidden | lg:block">
+        <div className="transition | pointer-fine:group-hover:-translate-y-6">
+          🚨 Where are your customers actually searching? Download the report
+        </div>
+        <div className="transition absolute top-0 left-0 translate-y-6 | pointer-fine:group-hover:translate-y-0">
+          🚨 Where are your customers actually searching? Download the report
+        </div>
+      </div>
+    </a>
+  );
+}
+
+export default TopAd;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Footer from "./components/Footer/Footer";
+import TopAd from "./components/TopAd/TopAd";
 
 function page() {
   return (
     <div className="px-4 md:px-7">
+      <TopAd />
       <Footer />
     </div>
   );
