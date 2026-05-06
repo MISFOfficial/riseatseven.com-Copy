95405c24f0df75798da3ef632535f9dfa9c87334
MISFOfficial <muksitul44@gmail.com>
2026-05-06 20:27:09 +0600

    feat: add new client work projects and refine WorkTitle component styling

--- app/components/FeatureWork/WorkTitle.tsx
+++ app/components/FeatureWork/WorkTitle.tsx
@@ -9,7 +9,12 @@ interface WorkTitleProps {
   onClick: () => void;
 }
 
-const WorkTitle: React.FC<WorkTitleProps> = ({ title, year, isActive, onClick }) => {
+const WorkTitle: React.FC<WorkTitleProps> = ({
+  title,
+  year,
+  isActive,
+  onClick,
+}) => {
   return (
     <div
       className={`relative transition | js-heading-40 ${
@@ -18,16 +23,12 @@ const WorkTitle: React.FC<WorkTitleProps> = ({ title, year, isActive, onClick })
       onClick={onClick}
     >
       <a
-        className={`flex items-start gap-x-2 transition-transform duration-500 cursor-pointer ${
-          isActive ? "translate-x-3" : ""
-        }`}
+        className={`flex items-start gap-x-2 transition-transform duration-500 cursor-pointer`}
       >
         <div className="inline-flex flex-wrap text-balance relative text-left justify-start text-white text-5xl/none | lg:text-6xl/none | xl:text-7xl/0.9 | 3xl:text-7.5xl/0.9 | 4xl:text-8xl/0.9 font-sans-primary font-medium tracking-tight js-heading">
           {title}
         </div>
-        <div className="text-white text-xs font-medium mt-2">
-          [{year}]
-        </div>
+        <div className="text-white text-xs font-medium mt-2">[{year}]</div>
       </a>
     </div>
   );
