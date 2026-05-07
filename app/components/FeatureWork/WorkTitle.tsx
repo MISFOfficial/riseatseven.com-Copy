374344925ed1c998799308b30f9c43c01a8be4da
MISFOfficial <muksitul44@gmail.com>
2026-05-08 00:33:18 +0600

    feat: update WorkTitle transition duration and adjust responsive typography sizing

--- app/components/FeatureWork/WorkTitle.tsx
+++ app/components/FeatureWork/WorkTitle.tsx
@@ -17,11 +17,11 @@ const WorkTitle: React.FC<WorkTitleProps> = ({
 }) => {
   return (
     <div
-      className={`relative transition-all duration-700 ease-out origin-left `}
+      className={`relative transition-all duration-200 ease-out origin-left hover:translate-x-3`}
       onClick={onClick}
     >
       <a className="flex items-start gap-x-2 transition-transform duration-500 cursor-pointer">
-        <div className="inline-flex flex-wrap text-balance relative text-left justify-start text-white text-6xl lg:text-7xl xl:text-8xl 3xl:text-9xl 4xl:text-[10rem] font-sans-primary font-medium tracking-tighter leading-none">
+        <div className="inline-flex flex-wrap text-balance relative text-left justify-start text-white text-6xl lg:text-7xl xl:text-[90px] font-sans-primary font-medium tracking-tighter leading-none">
           {title}
         </div>
         <div className="text-white text-xs font-medium mt-2">[{year}]</div>
