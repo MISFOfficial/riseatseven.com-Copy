80d42d489746b0f30d7d7091512e23cf86b56532
MISFOfficial <muksitul44@gmail.com>
2026-05-08 20:21:47 +0600

    style: remove red border from mobile insights image container

--- app/components/Insights/InsightsMobile.tsx
+++ app/components/Insights/InsightsMobile.tsx
@@ -60,7 +60,7 @@ function InsightsMobile({
                 });
               }}
             >
-              <div className="relative aspect-square rounded-4xl lg:rounded-[3rem] overflow-hidden bg-white border border-red-700">
+              <div className="relative aspect-square rounded-4xl lg:rounded-[3rem] overflow-hidden bg-white">
                 <Image
                   src={item.image}
                   alt={item.title}
